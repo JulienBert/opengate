0de7a029f69b95483f3f31338431f96269494685
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2023-03-29 14:49:22 +0200

    For windows compilation
    The templated function with specialization need to be inline

--- core/opengate_core/opengate_lib/digitizer/GateTDigiAttribute.h
+++ core/opengate_core/opengate_lib/digitizer/GateTDigiAttribute.h
@@ -15,43 +15,44 @@
 
 template <class T> class GateTDigiAttribute : public GateVDigiAttribute {
 public:
-  explicit GateTDigiAttribute(std::string vname);
+  explicit inline GateTDigiAttribute(std::string vname);
 
-  ~GateTDigiAttribute() override;
+  inline ~GateTDigiAttribute() override;
 
-  virtual int GetSize() const override;
+  virtual inline int GetSize() const override;
 
-  virtual std::vector<double> &GetDValues() override;
+  virtual inline std::vector<double> &GetDValues() override;
 
-  virtual std::vector<int> &GetIValues() override;
+  virtual inline std::vector<int> &GetIValues() override;
 
-  virtual std::vector<std::string> &GetSValues() override;
+  virtual inline std::vector<std::string> &GetSValues() override;
 
-  virtual std::vector<G4ThreeVector> &Get3Values() override;
+  virtual inline std::vector<G4ThreeVector> &Get3Values() override;
 
-  virtual std::vector<GateUniqueVolumeID::Pointer> &GetUValues() override;
+  virtual inline std::vector<GateUniqueVolumeID::Pointer> &
+  GetUValues() override;
 
-  const std::vector<T> &GetValues() const;
+  const inline std::vector<T> &GetValues() const;
 
-  virtual void FillToRoot(size_t index) const override;
+  virtual inline void FillToRoot(size_t index) const override;
 
-  virtual void FillDValue(double v) override;
+  virtual inline void FillDValue(double v) override;
 
-  virtual void FillSValue(std::string v) override;
+  virtual inline void FillSValue(std::string v) override;
 
-  virtual void FillIValue(int v) override;
+  virtual inline void FillIValue(int v) override;
 
-  virtual void Fill3Value(G4ThreeVector v) override;
+  virtual inline void Fill3Value(G4ThreeVector v) override;
 
-  virtual void FillUValue(GateUniqueVolumeID::Pointer v) override;
+  virtual inline void FillUValue(GateUniqueVolumeID::Pointer v) override;
 
-  virtual void Fill(GateVDigiAttribute *input, size_t index) override;
+  virtual inline void Fill(GateVDigiAttribute *input, size_t index) override;
 
-  virtual void FillDigiWithEmptyValue() override;
+  virtual inline void FillDigiWithEmptyValue() override;
 
-  virtual void Clear() override;
+  virtual inline void Clear() override;
 
-  virtual std::string Dump(int i) const override;
+  virtual inline std::string Dump(int i) const override;
 
 protected:
   struct threadLocal_t {
